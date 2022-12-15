3b1bef0ae60ec988722f8503c5e14dcf910786e1
nosnat <nosnat@gmail.com>
2022-12-16 01:09:50 +0200

    m1

--- src/src/main/java/com/handson/basic/repo/ShowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.handson.basic.repo;
-
-import com.handson.basic.model.Member;
-import com.handson.basic.model.Show;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShowRepository extends CrudRepository<Show,Long> {
-
-}
